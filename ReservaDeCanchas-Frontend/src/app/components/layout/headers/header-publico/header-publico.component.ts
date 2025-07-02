66365cd93f6e6206d9885393643f11948a0a8c84
ENJ23 <eliasjarma9@gmail.com>
2025-07-02 17:19:44 -0300

    Integracion header-publico

--- ReservaDeCanchas-Frontend/src/app/components/layout/headers/header-publico/header-publico.component.ts
+++ ReservaDeCanchas-Frontend/src/app/components/layout/headers/header-publico/header-publico.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-publico',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header-publico.component.html',
   styleUrl: './header-publico.component.css'
 })
