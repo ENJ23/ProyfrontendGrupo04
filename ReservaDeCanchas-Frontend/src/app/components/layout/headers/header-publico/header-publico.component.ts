9ca849d1f1873e351df4ffb92bfe69eeeced2c28
ENJ23 <eliasjarma9@gmail.com>
2025-07-02 17:12:57 -0300

    Mejora de accesibilidad Cliente-Encargado

---
+++ ReservaDeCanchas-Frontend/src/app/components/layout/headers/header-publico/header-publico.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-publico',
+  imports: [],
+  templateUrl: './header-publico.component.html',
+  styleUrl: './header-publico.component.css'
+})
+export class HeaderPublicoComponent {
+
+}
---
+++ ReservaDeCanchas-Frontend/src/app/models/usuario.model.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+  nombre: string;
+  apellido: string;
+  correo: string;
+  tipo: string;
+  userid?: string;
+} 
