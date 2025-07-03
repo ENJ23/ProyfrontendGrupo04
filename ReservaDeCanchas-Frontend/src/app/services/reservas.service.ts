eef1035d73ae882b616f31867fd4f9192f7141c1
Silva Jael <crwnberry@gmail.com>
2025-07-03 19:13:01 -0300

    Completar Pago

--- ReservaDeCanchas-Frontend/src/app/services/reservas.service.ts
+++ ReservaDeCanchas-Frontend/src/app/services/reservas.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -18,6 +18,15 @@ export class ReservasService {
     return this.http.get(`${this.apiUrl}/${id}`);
   }
 
+  getReservasPorClienteYFecha(nombreCliente: string, fecha?: string) {
+    let params = new HttpParams().set('nombre', nombreCliente);
+    
+    if (fecha) {
+      params = params.set('fecha', fecha);
+    }
+    
+    return this.http.get(`${this.apiUrl}/buscar`, { params });
+  }
   createReserva(data: any) {
     return this.http.post(this.apiUrl, data);
   }
