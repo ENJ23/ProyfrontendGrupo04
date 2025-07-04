bbb03fd0dd3dfd2ca24dc6d4ae915da12aed57a5
MAXSTREET-PC\PC <lucasandreu66@gmail.com>
2025-07-03 22:16:36 -0300

    Pequeñas Correciones

--- ReservaDeCanchas-Frontend/src/app/components/pages/inicio/inicio.component.ts
+++ ReservaDeCanchas-Frontend/src/app/components/pages/inicio/inicio.component.ts
@@ -1,10 +1,11 @@
 import { Component, AfterViewInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
